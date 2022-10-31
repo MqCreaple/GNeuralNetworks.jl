ca13f3bfd066f8cb1a6bfc29ae460fc820c25984
mqcreaple <tom.geng@prismsus.org>
2022-10-30 23:00:37 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/GNeuralNetworks.jl
@@ -0,0 +1,5 @@
+module GNeuralNetworks
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GNeuralNetworks
+using Test
+
+@testset "GNeuralNetworks.jl" begin
+    # Write your tests here.
+end
